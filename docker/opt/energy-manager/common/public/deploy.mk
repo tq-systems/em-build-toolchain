6cb35b0988573c57dd39c0ffe3c50bc443964de4
Christoph Krutz <christoph.krutz@tq-group.com>
2026-05-20 13:46:11 +0200

    Ensure that snapshots are deployed to a subdirectory derived from the branch
    The 'deploy-snapshot' target uses the branch as subdirectory.
    The 'deploy-release' target uses the tag as subdirectory, if available.
    This helps to keep the deployment folder structure organized.
    
    Signed-off-by: Christoph Krutz <christoph.krutz@tq-group.com>

--- docker/opt/energy-manager/common/public/deploy.mk
+++ docker/opt/energy-manager/common/public/deploy.mk
@@ -1,15 +1,20 @@
-ifndef TQEM_DEPLOY_SOURCE_PATH
-  $(error "TQEM_DEPLOY_SOURCE_PATH is not set")
+# By default, artifacts are deployed to a subfolder named after the branch
+export TQEM_DEPLOYMENT_SUBDIR ?= ${TQEM_GIT_BRANCH}
+
+# The subfolder ‘master’ is overwritten with ‘main’ in order to have a consistent deployment folder
+# structure. As main is the more common default branch name, this also results in a more intuitive
+# deployment folder structure.
+ifeq ($(TQEM_DEPLOYMENT_SUBDIR),master)
+  export TQEM_DEPLOYMENT_SUBDIR = main
 endif
 
-ifndef TQEM_DEPLOY_DESTINATION_PATH
-  $(error "TQEM_DEPLOY_DESTINATION_PATH is not set")
-endif
-
+# A snapshot deployment uses TQEM_GIT_BRANCH as subfolder in TQEM_DEPLOY_DESTINATION_PATH
 deploy-snapshot:
 	tqem-copy.sh ${TQEM_DEPLOY_SOURCE_PATH} ${TQEM_DEPLOY_DESTINATION_PATH} --overwrite
 
+# A release deployment uses TQEM_GIT_REFERENCE as subfolder in TQEM_DEPLOY_DESTINATION_PATH
 deploy-release:
+	$(eval TQEM_DEPLOYMENT_SUBDIR = ${TQEM_GIT_REFERENCE})
 	tqem-copy-safe.sh ${TQEM_DEPLOY_SOURCE_PATH} ${TQEM_DEPLOY_DESTINATION_PATH}
 
 .PHONY: deploy-snapshot deploy-release
